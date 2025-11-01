c95b55bab6f63c9ad1c93bb9b375fa9b6c737afe
Satyam Raj <57063364+satyamraj123@users.noreply.github.com>
2025-11-01 22:09:40 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,16 @@
+commands_and_descriptions={
+"/start" : "Initializes the bot; usually sends a welcome message or brief usage guide.",
+"/help" : "Lists all available commands and what they do.",
+"/about" : "Displays information about the bot (purpose, creator, etc.).",
+"/status" : "Returns bot status or uptime. Useful for checking if the bot is running.",
+"/stop" : "Ends the current interaction with the bot (optional)."
+}
+
+commands_and_response={
+"/start":"""👋 **Hey there!** Welcome to Sora Downloader Bot. I can help you download sora videos without watermark. Type `/help` to see what I can do.""",
+"/help":  """🧾 **How to use me?** Paste any sora video link in chat which should look like this: https://sora.chatgpt.com/ \n**Here’s what I can do:**  \n• `/start` — Start the bot  \n• `/help` — Lists all available commands and what they do.  \n• `/about` — Learn more about Sora Downloader Bot  \n• `/status` — Check if I’m active  \n• `/stop` — End the session""",
+"/about":  """🤖 **About this bot:**  \nI’m a simple Telegram bot built using Python. I can take a URL from you, process it, and return results. Created by Satyam and Lisa❤️""",
+"/status": """✅ **Bot is running smoothly!**  \nEverything looks good and ready to handle your requests.""",
+"/stop" : """👋 Alright, session ended. You can always type `/start` to begin again."""
+# "/run" 🚀 **Send me the URL** you want to process, and I’ll start working on it right away.                                                                                                                                             
+}
---
+++ handle_sora_videos.py
@@ -0,0 +1,85 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import json
+import traceback
+import asyncio
+
+WEBSITE = "https://savesora.com/sora-watermark-remover"
+
+#this function goes to this savesora website and scrapes sora video
+async def handle_sora_video(video_link: str):
+    print(rf'[INFO] Classified {video_link} as SORA !')
+    options = Options()
+    options.add_argument("--headless")
+    options.add_experimental_option("prefs", {
+        "safebrowsing.enabled": True
+    })
+    options.set_capability("goog:loggingPrefs", {"performance": "ALL"})  # enable network logs
+    driver = webdriver.Chrome(options=options)
+    wait = WebDriverWait(driver, 15)
+    try:
+        driver.get(WEBSITE)
+        time.sleep(3)
+
+        # Wait for textarea to appear
+        textarea = wait.until(EC.presence_of_element_located((By.ID, "link-input")))
+
+        #if the text area appears then copy the video link there
+        if textarea:
+            print("[SUCCESS] Textarea found !")
+            textarea.clear()
+            textarea.send_keys(video_link)
+            print("[SUCCESS] Textarea filled with video link.")
+        else:
+            print("[ERROR] Textarea not found — check your selector or frame.")
+            raise Exception("Textarea not found !")
+
+        # Wait for the first download button to become clickable (enabled)
+        print("[INFO] Waiting for download button to be enabled...")
+        wait.until(EC.element_to_be_clickable((By.ID, "download-btn")))
+        download_button = driver.find_element(By.ID, "download-btn")
+        print("[SUCCESS] First Download button is now enabled, clicking...")
+        download_button.click()
+        print("[INFO] First Download button clicked wait 5 seconds.")
+        time.sleep(5)
+
+        # Wait for video result container to appear
+        print("[INFO] Waiting for video result container to appear...")
+        wait.until(EC.presence_of_element_located((By.ID, "video-result-container")))
+        print("[SUCCESS] Video container appeared.")
+
+        # Wait for final “Download (No Watermark)” button
+        print("[INFO] Waiting for final download button to appear...")
+        final_btn = wait.until(EC.element_to_be_clickable((By.ID, "download-no-watermark-btn")))
+        print("[SUCCESS] Final download button is ready, clicking now.")
+        final_btn.click()
+        print("[INFO] Final Download button clicked wait 5 seconds.")
+        time.sleep(5)
+        
+        # Fetch network logs and extract downloadable video URL
+        logs = driver.get_log("performance")
+        video_url = None
+        for entry in logs:
+            try:
+                msg = json.loads(entry["message"])["message"]
+                if (msg["method"] == "Page.downloadWillBegin" and "url" in msg["params"].keys()):
+                    video_url = msg["params"]["url"]
+            except Exception:
+                continue
+        if not video_url:
+            raise Exception("[ERROR] Could not find video URL in network logs.")
+        else:
+            print("[SUCCESS] Found video URL:", video_url)
+
+        #return downloadable video url
+        return video_url
+    except Exception as e:
+        print(rf"[ERROR] {e}")
+        traceback.print_exc()
+        return "ERROR"
+    finally:
+        driver.quit()
